cf822a56cc6a8777762a1ebe057e9fb9f2923307
nickmac459 <57877649+nickmac459@users.noreply.github.com>
2020-04-01 01:14:24 -0500

    Minimizing S and maximizing V now.
    Changed r and h range also.

--- LightningTalkDemo.py
+++ LightningTalkDemo.py
@@ -13,24 +13,24 @@ def objectiveFunc(values):
     r = values[0]
     h = values[1]
     sh = (r**2 + h**2)**0.5
-    B = np.pi*(r**2)
     S = np.pi*r*sh
-    T = B + S
-    return [S,T]
+    V = (np.pi**(r**2)*h)/3
+    return [S,V]
 
 problem = Problem(2,2)
-problem.types[:] = [Real(4,10), Real(0,20)]
+problem.types[:] = [Real(0,2), Real(0,4)]
+problem.directions = [-1,1]
 problem.function = objectiveFunc
 
 algorithm = NSGAII(problem)
-algorithm.run(1)
+algorithm.run(10000)
 
 S_opt = [s.objectives[0] for s in algorithm.result]
-T_opt = [s.objectives[1] for s in algorithm.result]
+V_opt = [s.objectives[1] for s in algorithm.result]
 
-plt.scatter(S_opt, T_opt)
+plt.scatter(S_opt, V_opt)
 plt.xlabel('Surface Area')
-plt.ylabel('Total Area')
+plt.ylabel('Volume')
 plt.title('Offspring Population')
 plt.show()
 
