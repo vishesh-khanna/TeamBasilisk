1bb178cab4d314d93809781f7a5637a3b83ec18e
Vishesh Khanna <60115770+vishesh-khanna@users.noreply.github.com>
2020-04-26 16:48:55 -0500

    Update GeneticAlgoCubicTraj.py

--- GeneticAlgoCubicTraj.py
+++ GeneticAlgoCubicTraj.py
@@ -9,34 +9,67 @@ from platypus import NSGAII, Problem, Real
 import numpy as np
 import matplotlib.pyplot as plt
 
-def cubicTraj(values):
-    theta0 = 15
-    thetaf = 75
-    t0 = 0
-    tf = values[0]
+class TrajectoryPlanning:
+
+    def coefficents(self,order, theta0, thetaf,t0,tf):
+        a3 = (-2/tf**3)*(thetaf - theta0)
+        a2 = (3/tf**2)*(thetaf - theta0)
+        a1 = 0
+        a0 = theta0
+        array = [a3,a2,a1,a0]
+        return array
     
-    a3 = (-2/tf**3)*(thetaf - theta0)
-    a2 = (3/tf**2)*(thetaf - theta0)
-    a1 = 0
-    a0 = theta0
     
-    jerk = np.sqrt((6*a3)**2)
+    def OptimalParetoPoint(self,ParetoPoints):
+        #Plot the graph for ParetoFront
+        #Select the optimal Pareto Point
+        size = int(len(ParetoPoints)/2)
+        print(size)
+        ParetoPoint = ParetoPoints[size]
+        print(ParetoPoint)
+        print('Groot')
+        return ParetoPoint
     
-    return [tf,jerk]
+    def NSGAOperation(self,t0,theta0,thetaf):         
+        def Traj1(values):
+            tf_objective_values = values[0]
+            a3 = (-2/tf_objective_values**3)*(thetaf - theta0)
+            jerk = np.sqrt((6*a3)**2)
+            return [tf_objective_values,jerk]
 
-problem = Problem(1,2)
-problem.types[:] = Real(3,20)
-problem.directions = [-1,-1]
-problem.function = cubicTraj
+        problem = Problem(1,2)
+        problem.types[:] = Real(t0+3,20)
+        problem.directions = [-1,-1]
+        problem.function = Traj1
+        algorithm = NSGAII(problem)
+        algorithm.run(2000)
+        tf_opt = [s.objectives[0] for s in algorithm.result]
+        jerk_opt = [s.objectives[1] for s in algorithm.result]
+        
+        return tf_opt,jerk_opt
 
-algorithm = NSGAII(problem)
-algorithm.run(10000)
+Problem1 = TrajectoryPlanning()     
+order = 3
 
-tf_opt = [s.objectives[0] for s in algorithm.result]
-jerk_opt = [s.objectives[1] for s in algorithm.result]
-
-plt.scatter(tf_opt,jerk_opt)
-plt.show()
-
-tf = 5.23705
-jerk = 4.96
+path_points = [15,25,45,65,85]
+theta0 = path_points[0]
+thetaf = path_points[1]
+t0 = 0.01
+coefficients_via_points = []
+for i in range(len(path_points)+1):
+    tf_opt,jerk_opt = Problem1.NSGAOperation(t0,theta0,thetaf)
+    ParetoPoints1 = np.c_[np.array(tf_opt),np.array(jerk_opt)]
+    OptimalParetoPoint = Problem1.OptimalParetoPoint(ParetoPoints1)
+    tf = OptimalParetoPoint[0]
+    optimized_coefficients = Problem1.coefficents(order,theta0,thetaf,t0,tf)
+    if i ==len(path_points)-1:
+        break
+    theta0 = thetaf
+    thetaf = path_points[i+1]
+    t0 = tf    
+    coefficients_via_points.append(optimized_coefficients)
+print(coefficients_via_points)
+#Come out of for loop
+#We have coefficients for polynomials between the via points
+#    Plot the polynomial
+#    Plot the graphs
